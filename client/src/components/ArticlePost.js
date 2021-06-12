1651fa8bfa1424d3d0b64620d30747d6d877d314
Harman Hayer <hhhayer@gmail.com>
2021-06-12 16:16:02 -0700

    article post boilerplate

---
+++ client/src/components/ArticlePost.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const ArticlePost = () => {
+    return <h1>Article Post</h1>
+}
+export default ArticlePost;
---
+++ client/src/pages/CreateArticle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ArticlePost from '../components/ArticlePost';
+
+const CreateArticle = () => {
+
+    return <h1>Create Article</h1>
+}
+export default CreateArticle;
