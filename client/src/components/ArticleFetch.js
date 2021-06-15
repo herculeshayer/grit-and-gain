8d3fb11f864a7e81503311f9ad8593bcad94c1ac
Harman Hayer <hhhayer@gmail.com>
2021-06-14 18:08:52 -0700

    remove link spanning across page

--- client/src/components/ArticleFetch.js
+++ client/src/components/ArticleFetch.js
@@ -26,7 +26,7 @@ const ArticleGetAll = () => {
         const {_id, title} = article;
         return (
             <div key={key}>
-                <Link /*onClick={()=>setArticleData(articleData.filter(article => article._id === name))}*/ to={`/article/${_id}`}><h2>{title}</h2></Link>
+                <Link style={{display: "inline-block"}} to={`/article/${_id}`}><h2>{title}</h2></Link>
                 <br />
             </div>
         );
@@ -57,16 +57,16 @@ const ArticleGetOne = ({name}) => {
     const mapArticleData = articleData.map((article, key) => {
         const {title, data, author, comments, upvotes} = article;
         return(
-            <div key={key} style={StyleArticle}>
-                <h1>{title}</h1>
-                <h4>{author}</h4>
-                <h6>{upvotes}</h6>
-                <p>{comments.map((comment, key) => {
+            <div key={key} style={{StyleArticle}}>
+                <h1>Article: {title}</h1>
+                <h4>Created By: {author}</h4>
+                <h6>Upvotes: {upvotes}</h6>
+                <p>Comments: {comments.map((comment, key) => {
                     const { username, text } = comment;
                     return (
                         <div key={key}>
-                            <h1>{username}</h1>
-                            <p>{text}</p>
+                            <h1>Username: {username}</h1>
+                            <p>Text: {text}</p>
                         </div>
                     );
                 })}</p>
@@ -77,8 +77,7 @@ const ArticleGetOne = ({name}) => {
         );
     })
     return (
-        <>
-            <h1>ArticleGetOne{name}</h1>
+        <>  
             {mapArticleData}
         </>
     )
