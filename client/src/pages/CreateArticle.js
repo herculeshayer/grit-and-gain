5028bbc311e538103143be45ca6a8abb1eac1e29
Harman Hayer <hhhayer@gmail.com>
2021-06-14 17:48:34 -0700

    POST article info, create article modal

--- client/src/pages/CreateArticle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import ArticlePost from '../components/ArticlePost';
-
-const CreateArticle = () => {
-
-    return <h1>Create Article</h1>
-}
-export default CreateArticle;
