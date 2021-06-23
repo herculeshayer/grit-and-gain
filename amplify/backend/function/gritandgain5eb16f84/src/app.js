f49af60ff46fed04bbe9a99185d37cf1705031b4
Harman Hayer <hhhayer@gmail.com>
2021-06-23 13:04:53 -0700

    aws amplify serverless backend,basiccrud operation

---
+++ amplify/backend/function/gritandgain5eb16f84/src/app.js
@@ -0,0 +1,47 @@
+/*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+
+
+
+var express = require('express')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose');
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+// declare a new express app
+var app = express()
+app.use(express.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+const mongoAtlasURI = 'mongodb+srv://amplifyAdministratorUser:4ImUxgHzb1VpjCTb@clusterbustermuster.wv017.mongodb.net/articles?retryWrites=true&w=majority'
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
+mongoose.connect(mongoAtlasURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true });
+const db = mongoose.connection;
+db.on('error with db', (err)=>console.log(err));
+db.once('db open', ()=>console.log('successful connection'));
+
+
+app.use('/articles', require('./routes/Articles'))
+
+
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
---
+++ amplify/backend/function/gritandgain5eb16f84/src/models/Article.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    upvotes: {
+        type: Number,
+        required: false,
+        default: "1"
+    },
+    comments: [
+        {
+            username:{
+                type: String,
+                required: false
+            },
+            text: {
+                type: String,
+                required: false
+            }
+        },
+    ],
+    articleInfo: {
+        type: String,
+        required: false
+    },
+    date: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    }
+})
+module.exports = mongoose.model('article', articleSchema);
