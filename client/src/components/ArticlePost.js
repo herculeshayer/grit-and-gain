2fa520e6fb75e6b5d5f535caae5282ab777a7ae0
Harman Hayer <hhhayer@gmail.com>
2021-06-25 14:47:49 -0700

    add timerinterval, remove jssincss, add cors mode

--- client/src/components/ArticlePost.js
+++ client/src/components/ArticlePost.js
@@ -1,50 +1,59 @@
 import React, {useState} from 'react';
 
-const formContainer = {
-    position: "fixed",
-    top: "0",
-    left: "0",
-    right: "0",
-    bottom: "0",
-    backgroundColor: "rgba(0,0,0,0.5)",
-    display: "flex",
-    alignItems: "center",
-    justifyContext: "center",
+// const formContainer = {
+//     position: "fixed",
+//     top: "0",
+//     left: "0",
+//     right: "0",
+//     bottom: "0",
+//     backgroundColor: "rgba(0,0,0,0.5)",
+//     display: "flex",
+//     alignItems: "center",
+//     justifyContext: "center",
     
-    // opacity:"0",
-    // transition: "all 0.3s ease-in-out"
-}
+//     // opacity:"0",
+//     // transition: "all 0.3s ease-in-out"
+// }
 
-const content = { 
-    width: "40%",
-    backgroundColor: "white",
-    margin: "0 auto",
-    borderRadius: "25px"
-}
+// const content = { 
+//     width: "40%",
+//     backgroundColor: "white",
+//     margin: "0 auto",
+//     borderRadius: "25px"
+// }
 
-const body = {
-    textAlign: "center"
-}
+// const body = {
+//     textAlign: "center"
+// }
 
-const footer = {
-    textAlign: "center",
-    padding: "20px",
-}
+// const footer = {
+//     textAlign: "center",
+//     padding: "20px",
+// }
 
 const ArticlePost = ({closeModal}) => {
     const [ title, setTitle ] = useState('');
     const [ author, setAuthor ] = useState('');
-    const [ articleInfo, setarticleInfo ] = useState('');
+    const [ articleInfo, setArticleInfo ] = useState('');
     
     const addArticle = async() => {
-            fetch('https://oj4m71cxjh.execute-api.us-west-2.amazonaws.com/dev/articles', {
+            await fetch('https://oj4m71cxjh.execute-api.us-west-2.amazonaws.com/dev/articles', {
             method: 'POST',
-            body: JSON.stringify({title, author, articleInfo}),
+            mode: 'cors',
+            body: JSON.stringify({
+                title: title, 
+                author: author, 
+                articleInfo: articleInfo
+            }),
             headers: {
                 'Content-Type': 'application/json'
             }
         })
+        .then(res=>res.json())
+        .then(data=>console.log(data))
+        .catch(err=>console.log(err))
     }
+    
     
     const handleSubmit = e => {
         e.preventDefault();
@@ -53,60 +62,65 @@ const ArticlePost = ({closeModal}) => {
 
         setTitle('');
         setAuthor('');
-        setarticleInfo('');
+        setArticleInfo('');
         closeModal();
-        (()=>window.location.reload())()
+
+        //refresh page after 100ms upon form submit
+        setTimeout(()=> {
+            window.location.reload()
+        }, 100);
+        
     }
     
     return (
-        <div style={formContainer} onClick={closeModal}>
-            <form style={content} onSubmit={handleSubmit} onClick={(e)=>e.stopPropagation()}>
-                <h1 style={{paddingBottom: "20px", textAlign: "center"}}>Create Article</h1>
-                <div style={body}>
-                    <label>Title: </label>
-                    <input
-                        style={{width: "60%", maxWidth: "100%"}} 
-                        type="text" 
-                        value={title} 
-                        placeholder="How to Squat with Correct Form" 
-                        onChange={(e)=>setTitle(e.target.value)} 
-                        required
-                        
-                    />
-                    <br/>
-                    <label>Author: </label>
-                    <input 
-                        style={{width: "60%", maxWidth: "100%"}} 
-                        type="name" 
-                        value={author} 
-                        placeholder="John Doe" 
-                        onChange={(e)=>setAuthor(e.target.value)} 
-                        required
-                    />
-                    <br />
-                    <label>Article Info: </label>
-                    <br />
-                    <textarea 
-                        style={{resize: "none", maxWidth: "100%"}} 
-                        rows="20" 
-                        cols="50" 
-                        value={articleInfo} 
-                        placeholder="Keep correct form" 
-                        onChange={(e)=>setarticleInfo(e.target.value)} 
-                        required
-                    />
-                </div>
-                <div style={footer}>
-                    <button 
-                        type="submit" 
-                        style={{width: "4rem", height: "2rem"}}>
-                            Submit</button>
-                    <button 
-                        type="reset" 
-                        style={{width: "4rem", height: "2rem"}} 
-                        onClick={closeModal}>
-                            Cancel</button>
-                </div>
+        <div className="formContainer" onClick={closeModal}>
+            <form className="content" onSubmit={handleSubmit} onClick={(e)=>e.stopPropagation()}>
+                    <h1 style={{paddingBottom: "20px", textAlign: "center"}}>Create Article</h1>
+                    <div className="bodyStyle">
+                        <label>Title: </label>
+                        <input
+                            style={{width: "60%", maxWidth: "100%"}} 
+                            type="text" 
+                            value={title} 
+                            placeholder="How to Squat with Correct Form" 
+                            onChange={(e)=>setTitle(e.target.value)} 
+                            required
+                            
+                        />
+                        <br/>
+                        <label>Author: </label>
+                        <input 
+                            style={{width: "60%", maxWidth: "100%"}} 
+                            type="name" 
+                            value={author} 
+                            placeholder="John Doe" 
+                            onChange={(e)=>setAuthor(e.target.value)} 
+                            required
+                        />
+                        <br />
+                        <label>Article Info: </label>
+                        <br />
+                        <textarea 
+                            style={{resize: "none", maxWidth: "100%"}} 
+                            rows="20" 
+                            cols="50" 
+                            value={articleInfo} 
+                            placeholder="Keep correct form" 
+                            onChange={(e)=>setArticleInfo(e.target.value)} 
+                            required
+                        />
+                    </div>
+                    <div className="footerStyle">
+                        <button 
+                            type="submit" 
+                            style={{width: "4rem", height: "2rem"}}>
+                                Submit</button>
+                        <button 
+                            type="reset" 
+                            style={{width: "4rem", height: "2rem"}} 
+                            onClick={closeModal}>
+                                Cancel</button>
+                    </div>
             </form>
         </div>
     );
