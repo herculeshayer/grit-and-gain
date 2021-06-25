f96bc88ae84f9e32546d1e14a85a0ba96736fdbb
Harman Hayer <hhhayer@gmail.com>
2021-06-25 14:44:38 -0700

    create modal for edit article

--- client/src/pages/DisplaySingleArticle.js
+++ client/src/pages/DisplaySingleArticle.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Context } from 'react';
 
 import { ArticleGetOne, ArticleGetAll } from '../components/ArticleFetch';
 import ArticleDelete from '../components/ArticleDelete';
+import ArticlePatch from '../components/ArticlePatch';
 
 
 const DisplaySingleArticle = ({match}) => {
+    const [ showModal, setShowModal ] = useState(false);
     const name = match.params.id;
    
     return (
         <article>
-            <ArticleGetOne name={name}/>
-            <ArticleDelete name={name}/>
+            <ArticleDelete name={name} />
+            <button onClick={()=>setShowModal(true)}>Edit Article</button>
+            {showModal && <ArticlePatch name={name} closeModal={()=>setShowModal(false)}/>}
+            <ArticleGetOne name={name} />
+            
         </article>
     );
 }
