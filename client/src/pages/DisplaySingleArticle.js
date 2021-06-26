74562d3854a15ad4e89c4f75fb5ccbdb0f71244a
Harman Hayer <hhhayer@gmail.com>
2021-06-26 11:54:39 -0700

    display modal,pass useState as prop to commentPost

--- client/src/pages/DisplaySingleArticle.js
+++ client/src/pages/DisplaySingleArticle.js
@@ -4,18 +4,25 @@ import { Context } from 'react';
 import { ArticleGetOne, ArticleGetAll } from '../components/ArticleFetch';
 import ArticleDelete from '../components/ArticleDelete';
 import ArticlePatch from '../components/ArticlePatch';
+import CommentPost from '../components/CommentPost';
 
 
 const DisplaySingleArticle = ({match}) => {
     const [ showModal, setShowModal ] = useState(false);
+    const [showCommentModal, setCommentModal ] = useState(false);
     const name = match.params.id;
    
     return (
         <article>
             <ArticleDelete name={name} />
+
             <button onClick={()=>setShowModal(true)}>Edit Article</button>
             {showModal && <ArticlePatch name={name} closeModal={()=>setShowModal(false)}/>}
+            
             <ArticleGetOne name={name} />
+
+            <button onClick={()=>setCommentModal(true)}>Add Comment</button>
+            {showCommentModal && <CommentPost name={name} closeModal={()=>setCommentModal(false)}/>}
             
         </article>
     );
