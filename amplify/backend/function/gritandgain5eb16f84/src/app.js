6ee841d16f69d59f18abbc08c54183556d7d9401
Harman Hayer <hhhayer@gmail.com>
2021-06-26 11:50:26 -0700

    useFindandModifty : false - correct setting

--- amplify/backend/function/gritandgain5eb16f84/src/app.js
+++ amplify/backend/function/gritandgain5eb16f84/src/app.js
@@ -27,7 +27,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "*")
   next()
 });
-mongoose.connect(`${process.env.MONGO_ATLAS}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true });
+mongoose.connect(`${process.env.MONGO_ATLAS}`, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 const db = mongoose.connection;
 db.on('error with db', (err)=>console.log(err));
 db.once('db open', ()=>console.log('successful connection'));
