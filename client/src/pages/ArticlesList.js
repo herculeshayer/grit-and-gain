0bbcee5f758f9deff60079a8b9f4f87d590e9a15
Harman Hayer <hhhayer@gmail.com>
2021-07-01 17:16:30 -0700

    getall article list css

--- client/src/pages/ArticlesList.js
+++ client/src/pages/ArticlesList.js
@@ -3,19 +3,35 @@ import React, {useState} from 'react';
 import { ArticleGetAll } from '../components/ArticleGet';
 import ArticlePost from '../components/ArticlePost';
 
+const ArticleTitle = {
+    margin: "0px auto 0px auto",
+    textAlign: "center",
+    display: "block"
+}
+const ArticleContainer = {
+    backgroundColor: "white",
+    margin: "0px auto 0px auto",
+    width: "30%",
+    borderRadius: "15%",
+    border: "solid black 3px"
+
+}
+
 const ArticlesList = () => {
     const [showModal, setShowModal] = useState(false);
 
   
     return (
         <>
-            <h3>This is the articles page</h3>
-            <p>Below are a few articles which you can view</p>
-            <div>
-                <button style={{marginBottom: "18vh"}} onClick={()=> setShowModal(true)}>Create Article</button>
+            <section style={ArticleTitle}>
+                <h3>This is the articles page</h3>
+                <p>Below are a few articles which you can view</p>
+                <button style={{marginBottom: "18vh", marginTop: "3vh"}} onClick={()=> setShowModal(true)}>Create Article</button>
+            </section>
+            <div style={ArticleContainer}>
                 {showModal && <ArticlePost closeModal={()=>setShowModal(false)}/>}
+                <ArticleGetAll />
             </div>
-            <ArticleGetAll />
         </>
         
     );
